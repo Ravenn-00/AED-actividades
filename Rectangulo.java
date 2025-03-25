a3f7f59293c87f79a29b1a5b4399d53d1e9b45cc
Rav <freze000@gmail.com>
2025-03-25 00:40:39 -0500

    fixes 3

--- Rectangulo.java
+++ Rectangulo.java
@@ -2,10 +2,10 @@ public class Rectangulo {
     private Coordenada esquina1;
     private Coordenada esquina2;
 
-    // public Rectangulo(Coordenada c1, Coordenada c2) {
-    //     setEsquina1(C1);
-    //     setEsquina2(C2);
-    // }
+    public Rectangulo(Coordenada c1, Coordenada c2) {
+        setEsquina1(c1);
+        setEsquina2(c2);
+    }
     public void setEsquina1(Coordenada c) {
         this.esquina1 = c;
     }
@@ -18,7 +18,19 @@ public class Rectangulo {
     public Coordenada getEsquina2() {
         return esquina2;        
     }
-    // public String toString() {
-
-    // }
+    public int getMinX() {
+        return Math.min(esquina1.getX(), esquina2.getX());
+    }
+    public int getMaxX() {
+        return Math.max(esquina1.getX(), esquina2.getX());
+    }
+    public int getMinY() {
+        return Math.min(esquina1.getY(), esquina2.getY());
+    }
+    public int getMaxY() {
+        return Math.max(esquina1.getY(), esquina2.getY());
+    }
+    public String toString() {
+        return "Esquinas: " + esquina1 + " y " + esquina2;
+    }
 }
--- Verificador.java
+++ Verificador.java
@@ -1,3 +1,23 @@
 public class Verificador {
-    
-}
+    public static void verificarRelacion(Rectangulo R1, Rectangulo R2) {
+        int R1MinX = R1.getMinX();
+        int R1MaxX = R1.getMaxX();
+        int R1MinY = R1.getMinY();
+        int R1MaxY = R1.getMaxY();
+
+        int R2MinX = R2.getMinX();
+        int R2MaxX = R2.getMaxX();
+        int R2MinY = R2.getMinY();
+        int R2MaxY = R2.getMaxY();
+
+        if(R1MinX < R2MaxX && R1MaxX > R2MinX && R1MinY < R2MaxY && R1MaxY > R2MinY) {
+            System.out.print("se sobreponen");
+        }
+        if(R1MinX == R2MaxX || R2MinX == R1MaxX || R1MinY ==  R2MaxY || R2MinY == R1MaxY) {
+            System.out.print("estan juntos");
+        }
+        else {
+            System.out.print("son disjuntos");
+        }
+    }
+}    
